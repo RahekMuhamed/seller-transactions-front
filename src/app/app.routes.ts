8a87976cd668a809fb9f08726fcdd66703779b8b
RahekMuhamed <raheksalem@gmail.com>
2025-07-10 21:36:43 +0300

    Add routing for transactions and sales-summary

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { TransactionsComponent } from './components/transactions/transactions.component';
+import { ChartComponent } from './components/chart/chart.component';
+
+export const routes: Routes = [
+  { path: 'transactions', component: TransactionsComponent },
+  { path: 'sales-summary', component: ChartComponent },
+];
